fa6e8d90fa8665dce1c87f7cb894e7c8dc763b56
ragy <wad.moskla@gmail.com>
2020-04-16 12:22:26 +0200

    onclite: Include Gcam Go at least better then Snap

--- device.mk
+++ device.mk
@@ -128,7 +128,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     libgui_vendor \
-    Snap
+    GCam
 
 # Configstore
 PRODUCT_PACKAGES += \
